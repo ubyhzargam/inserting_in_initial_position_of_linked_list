e7c930b9cd1c3cfa42425ea781fb2f193fe9c9ec
ubyhzargam <ubyhzargam@gmail.com>
2023-07-15 12:41:58 +0530

    Initial Commit

---
+++ inserting_in_initial_position_of_linked_list/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  inserting_in_initial_position_of_linked_list
+//
+//  Created by Uby H on 15/07/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
